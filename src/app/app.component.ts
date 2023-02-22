d4154d3113a5066aa81b05146567e483484bfbf6
hp_computer <andrademareshp@gmail.com>
2023-02-21 23:49:48 -0600

    Property binding - renderizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  nombre = 'Antonio';
+  age = 25;
+  btnDisabled = true;
+  imgTest ='https://i.pinimg.com/736x/48/51/60/485160c55c513f8eac38027385f38a52.jpg'
+
+  persona = {
+    name:'Bruce',
+    lastname:'Wayne',
+    address:{
+      calle: 'Gotham',
+      numero:19
+    }
+  }
 }
---
+++ src/assets/notes.md
@@ -0,0 +1,5 @@
+# run app in other port
+ng serve -o --port=3500
+
+# ng version
+- dentro de la aplicación podemos ver las dependencias de la aplicación
