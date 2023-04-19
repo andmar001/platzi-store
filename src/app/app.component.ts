8a7ec45b83d94df46f70f8198fd53ac52fe56331
hp_computer <andrademareshp@gmail.com>
2023-04-18 23:59:35 -0600

    input - padre a hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,45 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'platzi-store';
 
-  nombre = 'Antonio';
-  age = 25;
-  btnDisabled = true;
-  imgTest ='https://i.pinimg.com/736x/48/51/60/485160c55c513f8eac38027385f38a52.jpg'
+  imgParent :string = 'https://www.w3schools.com/howto/img_avatar.png'
 
-  persona = {
-    name:'Bruce',
-    lastname:'Wayne',
-    address:{
-      calle: 'Gotham',
-      numero:19
-    }
-  }
-
-  banderaBoton = true;
-  //cambiare el estado de los botones
-  toggleButton(){
-    this.banderaBoton = !this.banderaBoton; // negar el valor boolerano q tenga en ese momento
-  }
-
-  //aumentar Edad
-  increaseAge(){
-    this.age = this.age + 1
-  }
-
-  // event:Event ---- evento nativo
-  //leer el scroll -  metodo scroll
-  onScroll(event:Event){
-    const element = event.target as HTMLElement
-    console.log(element.scrollTop)
-  }
-
-  //leer eventos de un input
-  changeName(event:Event){
-    // HTMLInputElement porque estamos trabajando con elemento - etiqueta input
-    const element = event.target as HTMLInputElement;
-    element.value;
-    this.persona.name = element.value;
-  }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.css']
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit{
 
+  @Input() img: string = "valor inicial";
+
+  ngOnInit(): void {
+
+  }
 }
