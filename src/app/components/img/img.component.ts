984db8e77a1a282054f78c91e56a8c009ad1b281
hp_computer <andrademareshp@gmail.com>
2023-04-20 00:11:45 -0600

    Ciclo de vida de los componentes}

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, OnChanges, SimpleChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.css']
 })
-export class ImgComponent implements OnInit{
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy{
 
   @Input() img: string = "valor inicial";
 
@@ -13,8 +13,33 @@ export class ImgComponent implements OnInit{
 
   imageDefault = "../../../assets/img/img_nothing.jpg";
 
-  ngOnInit(): void {
+  constructor() {
+    // before render
+    // no correr cosas asincronas ---once time
+    console.log("constructor called =>", this.img);
+  }
 
+  ngOnChanges(): void {
+    // before render - during the render
+    //cambios en los inputs - se ejecuta las veces q se actualizen los inputs que definamos
+    console.log("on changes => imgValue", this.img)
+
+  }
+  ngOnInit(): void {
+    // before render
+    // podemos correr cosas de manera asincrona - corre una sola vez
+    console.log("on init => imgValue", this.img)
+  }
+
+  ngAfterViewInit(): void {
+    //after render - despues de que se renderizan componentes hijos
+    // handler children - manejador de componentes hijos
+    console.log("afterview init => imgValue");
+  }
+
+  ngOnDestroy(): void {
+    // donde se elimina el componente
+    console.log("ng on destroy");
   }
 
   imgError(){
